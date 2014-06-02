8891bb7384d99c78f1bf306f314a93e6da23bd16
Montel Laurent <montel@kde.org>
2014-06-02 20:04:04 +0200

    Add KPluralHandlingSpinbox to allow to add plural i18n when we port KIntSpinBox
    Review and ok'ed by David.
    Use it just when we need to add suffix with plural otherwise use QSpinBox
    
    CCMAIL: mgraesslin@kde.org

---
+++ autotests/kpluralhandlingspinboxtest.cpp
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2014 Laurent Montel <montel@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#include "kpluralhandlingspinboxtest.h"
+#include "kpluralhandlingspinbox.h"
+
+#include <QTest>
+
+QTEST_MAIN(KPluralHandlingSpinBoxTest)
+
+KPluralHandlingSpinBoxTest::KPluralHandlingSpinBoxTest()
+{
+}
+
+void KPluralHandlingSpinBoxTest::shouldHaveDefautValue()
+{
+    KPluralHandlingSpinBox spinbox;
+    QCOMPARE(spinbox.suffix(), QString());
+}
+
+void KPluralHandlingSpinBoxTest::shouldUseSingularValueWhenUseValueEqualToOne()
+{
+    KPluralHandlingSpinBox spinbox;
+    spinbox.setSuffix(ki18np("singular", "plural"));
+    spinbox.setValue(1);
+    QCOMPARE(spinbox.suffix(), QLatin1String("singular"));
+}
+
+void KPluralHandlingSpinBoxTest::shouldUsePlurialValueWhenUseValueSuperiorToOne()
+{
+    KPluralHandlingSpinBox spinbox;
+    spinbox.setSuffix(ki18np("singular", "plural"));
+    spinbox.setValue(2);
+    QCOMPARE(spinbox.suffix(), QLatin1String("plural"));
+}
+
+void KPluralHandlingSpinBoxTest::shouldUseSingularValueWhenWeChangeValueAndFinishWithValueEqualOne()
+{
+    KPluralHandlingSpinBox spinbox;
+    spinbox.setSuffix(ki18np("singular", "plural"));
+    spinbox.setValue(2);
+    spinbox.setValue(1);
+    QCOMPARE(spinbox.suffix(), QLatin1String("singular"));
+    QCOMPARE(spinbox.value(), 1);
+}
+
+void KPluralHandlingSpinBoxTest::shouldReturnEmptySuffix()
+{
+    KPluralHandlingSpinBox spinbox;
+    spinbox.setValue(2);
+    QCOMPARE(spinbox.suffix(), QString());
+}
+
+void KPluralHandlingSpinBoxTest::shouldReturnDefaultSuffixWhenUseQSpinBoxDefaultSuffixFunction()
+{
+    KPluralHandlingSpinBox spinbox;
+    spinbox.setSuffix(QLatin1Literal("suffix"));
+    spinbox.setValue(2);
+    QCOMPARE(spinbox.suffix(), QLatin1String("suffix"));
+    spinbox.setValue(1);
+    QCOMPARE(spinbox.suffix(), QLatin1String("suffix"));
+}
+
