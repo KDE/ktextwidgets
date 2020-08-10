63d46ad396c3a747731c514be481163ba956cbbc
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-08-10 12:00:45 +0200

    Convert copyright statements to SPDX expressions

--- autotests/kfindtest.cpp
+++ autotests/kfindtest.cpp
@@ -1,23 +1,11 @@
 /*
-    Copyright (C) 2004, Arend van Beelen jr. <arend@auton.nl>
-    Copyright (C) 2010, David Faure <faure@kde.org>
-
     This file is part of the KDE project
+    SPDX-FileCopyrightText: 2004 Arend van Beelen jr. <arend@auton.nl>
+    SPDX-FileCopyrightText: 2010 David Faure <faure@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License version 2, as published by the Free Software Foundation.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LGPL-2.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+    SPDX-License-Identifier: LGPL-2.0-only
 */
+
 #include "kfindtest.h"
 
 #include <QRegularExpression>
--- src/widgets/krichtextedit.h
+++ src/widgets/krichtextedit.h
@@ -1,25 +1,11 @@
 /*
- * krichtextedit.h
- *
- * Copyright 2007 Laurent Montel <montel@kde.org>
- * Copyright 2008 Thomas McGuire <thomas.mcguire@gmx.net>
- * Copyright 2008 Stephen Kelly  <steveire@gmail.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301  USA
- */
+    krichtextedit.h
+    SPDX-FileCopyrightText: 2007 Laurent Montel <montel@kde.org>
+    SPDX-FileCopyrightText: 2008 Thomas McGuire <thomas.mcguire@gmx.net>
+    SPDX-FileCopyrightText: 2008 Stephen Kelly <steveire@gmail.com>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
 
 #ifndef KRICHTEXTEDIT_H
 #define KRICHTEXTEDIT_H
